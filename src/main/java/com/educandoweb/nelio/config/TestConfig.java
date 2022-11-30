aae43b0472317635c7a374af104ffd85b4aa7540
Ana Carolina Silveira <ana.cfbsilveira@gmail.com>
2022-11-30 20:12:39 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/nelio/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.educandoweb.nelio.config;
+
+import com.educandoweb.nelio.entities.User;
+import com.educandoweb.nelio.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test") // nome idêntico ao inserido em application.properties -> spring.profiles.active=test
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
