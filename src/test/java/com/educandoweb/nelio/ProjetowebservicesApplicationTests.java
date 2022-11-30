f886d1a4ad4f8fc906142897868cf5b52ebcc6a5
Ana Carolina Silveira <ana.cfbsilveira@gmail.com>
2022-11-30 18:03:48 -0300

    Criação do projeto

---
+++ src/test/java/com/educandoweb/nelio/ProjetowebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.nelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
