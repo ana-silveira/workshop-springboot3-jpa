4f6a7325ec35822b4a0406a6259ed536558c2fcf
Ana Carolina Silveira <ana.cfbsilveira@gmail.com>
2022-12-01 11:00:46 -0300

    Order status Enum

--- src/main/java/com/educandoweb/nelio/entities/Order.java
+++ src/main/java/com/educandoweb/nelio/entities/Order.java
@@ -1,5 +1,7 @@
 package com.educandoweb.nelio.entities;
 
+import com.educandoweb.nelio.enums.OrderStatus;
+import com.educandoweb.nelio.services.OrderService;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -21,6 +23,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     //@JsonIgnore ---> Se eu quiser que o JPA busque todos os orders associados a um user.
     @ManyToOne
     @JoinColumn(name = "client_id ")
@@ -29,9 +33,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,6 +54,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
