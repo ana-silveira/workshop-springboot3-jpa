dc10fd05c48fb9d97aaa70dcacb5a4883ed24902
Ana Carolina Silveira <ana.cfbsilveira@gmail.com>
2022-12-02 22:43:57 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/nelio/services/UserService.java
+++ src/main/java/com/educandoweb/nelio/services/UserService.java
@@ -4,6 +4,7 @@ import com.educandoweb.nelio.entities.User;
 import com.educandoweb.nelio.repositories.UserRepository;
 import com.educandoweb.nelio.services.exceptions.DatabaseException;
 import com.educandoweb.nelio.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -44,9 +45,13 @@ public class UserService {
 
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
