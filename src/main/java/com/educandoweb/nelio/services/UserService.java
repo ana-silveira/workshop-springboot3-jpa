2a5adc623ce98b8dc04c7f2c97712a973e73c752
Ana Carolina Silveira <ana.cfbsilveira@gmail.com>
2022-12-02 22:37:11 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/nelio/services/UserService.java
+++ src/main/java/com/educandoweb/nelio/services/UserService.java
@@ -2,9 +2,12 @@ package com.educandoweb.nelio.services;
 
 import com.educandoweb.nelio.entities.User;
 import com.educandoweb.nelio.repositories.UserRepository;
+import com.educandoweb.nelio.services.exceptions.DatabaseException;
 import com.educandoweb.nelio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,8 +33,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
+
 
     public User update(Long id, User obj) {
         User entity = repository.getReferenceById(id);
